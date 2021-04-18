44d2f79629216e6005db6f0443a0e6bb5f8f3232
Mohammed Ali Jaafar <mohammedalijaafar1999@gmail.com>
2021-04-18 11:49:51 +0300

    first commit

---
+++ Product.php
@@ -0,0 +1,272 @@
+<?php
+include_once "./Database.php";
+class Product
+{
+
+    private $productCode;
+    private $productName;
+    private $productLine;
+    private $productScale;
+    private $productVendor;
+    private $productDescription;
+
+    public static $rowsPerPage = 10;
+
+    public function __construct()
+    {
+        $this->productCode = null;
+        $this->productName = null;
+        $this->productLine = null;
+        $this->productScale = null;
+        $this->productVendor = null;
+        $this->productDescription = null;
+    }
+
+    /**
+     * Get the value of productCode
+     */
+    public function getProductCode()
+    {
+        return $this->productCode;
+    }
+
+    /**
+     * Set the value of productCode
+     *
+     * @return  self
+     */
+    public function setProductCode($productCode)
+    {
+        $this->productCode = $productCode;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of productName
+     */
+    public function getProductName()
+    {
+        return $this->productName;
+    }
+
+    /**
+     * Set the value of productName
+     *
+     * @return  self
+     */
+    public function setProductName($productName)
+    {
+        $this->productName = $productName;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of productLine
+     */
+    public function getProductLine()
+    {
+        return $this->productLine;
+    }
+
+    /**
+     * Set the value of productLine
+     *
+     * @return  self
+     */
+    public function setProductLine($productLine)
+    {
+        $this->productLine = $productLine;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of productScale
+     */
+    public function getProductScale()
+    {
+        return $this->productScale;
+    }
+
+    /**
+     * Set the value of productScale
+     *
+     * @return  self
+     */
+    public function setProductScale($productScale)
+    {
+        $this->productScale = $productScale;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of productVendor
+     */
+    public function getProductVendor()
+    {
+        return $this->productVendor;
+    }
+
+    /**
+     * Set the value of productVendor
+     *
+     * @return  self
+     */
+    public function setProductVendor($productVendor)
+    {
+        $this->productVendor = $productVendor;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of productDescription
+     */
+    public function getProductDescription()
+    {
+        return $this->productDescription;
+    }
+
+    /**
+     * Set the value of productDescription
+     *
+     * @return  self
+     */
+    public function setProductDescription($productDescription)
+    {
+        $this->productDescription = $productDescription;
+
+        return $this;
+    }
+
+    public function initWithId($id)
+    {
+        # code...
+    }
+
+    static public function getAll()
+    {
+        $conn = Database::getInstance();
+        $mysqli = $conn->getConnection();
+
+        $results = $mysqli->query("select SQL_CALC_FOUND_ROWS * from products");
+
+        $objects = [];
+        if ($results) {
+            while ($row = $results->fetch_object()) {
+                $objects[] = $row;
+            }
+            return $objects;
+        } else {
+            echo "Something went wrong";
+            return false;
+        }
+    }
+
+    // static public function basicSearch($query)
+    // {
+    //     $conn = Database::getInstance();
+    //     $mysqli = $conn->getConnection();
+
+    //     //$results = $mysqli->query("select * from products where productName = '$query'");
+    //     $results = $mysqli->query("select * from products where match (productName, productCode) against('$query' IN NATURAL LANGUAGE MODE)");
+
+    //     $objects = [];
+    //     if ($results) {
+    //         while ($row = $results->fetch_object()) {
+    //             $objects[] = $row;
+    //             echo $row->productCode . " - " . $row->productName . " - " . $row->productLine . "<br/>";
+    //         }
+    //         return $objects;
+    //     } else {
+    //         echo "Something went wrong";
+    //         return false;
+    //     }
+    // }
+
+    static public function getProductLines()
+    {
+        $conn = Database::getInstance();
+        $mysqli = $conn->getConnection();
+
+        $results = $mysqli->query("select * from productlines");
+        $productLines = [];
+
+        if ($results) {
+            while ($row = $results->fetch_assoc()) {
+                $productLines[] = $row["productLine"];
+            }
+            return $productLines;
+        } else {
+            return false;
+        }
+    }
+
+    static public function advancedSearch($query = null, $productLine = null)
+    {
+
+        //get selected page
+        $pageNum = 1;
+
+        if (isset($_GET["page"]) && !empty($_GET["page"])) {
+            $pageNum = intval($_GET["page"]);
+        }
+
+        //get instance
+        $conn = Database::getInstance();
+        $mysqli = $conn->getConnection();
+
+        //basic query
+        $sql = "select SQL_CALC_FOUND_ROWS * from products where 1=1";
+
+        //add search query if provided
+        if ($query != null) {
+            $sql .= " and match (productName, productCode) against('$query' IN NATURAL LANGUAGE MODE)";
+        }
+
+        //add product line if provided
+        if ($productLine != null) {
+            $sql .= " and productLine = '$productLine'";
+        }
+
+        //calculate the rows per page
+        $startRows = ($pageNum - 1) * 5;
+        $totalRowsPerPage = Product::$rowsPerPage;
+        $sql .= " limit $startRows,$totalRowsPerPage";
+
+        //to see the final sql result
+        echo $sql . "<br><br>";
+
+        //obvious stuff that don't need explanation
+        $results = $mysqli->query($sql);
+
+        $objects = [];
+        if ($results) {
+            while ($row = $results->fetch_object()) {
+                $objects[] = $row;
+                //echo $row->productCode . " - " . $row->productName . " - " . $row->productLine . "<br/>";
+            }
+            return $objects;
+        } else {
+            echo "Something went wrong";
+            return false;
+        }
+    }
+
+    static public function getSelectCount()
+    {
+        $conn = Database::getInstance();
+        $mysqli = $conn->getConnection();
+
+        $sql = "SELECT FOUND_ROWS();";
+
+        $result = $mysqli->query("SELECT FOUND_ROWS();");
+
+        $row = $result->fetch_row();
+
+        return intval($row[0]);
+    }
+}
---
+++ index.php
@@ -0,0 +1,101 @@
+<?php
+include_once "./Product.php";
+$results = null;
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles.css">
+    <title>Basic Search</title>
+</head>
+
+<body>
+    <form method="get">
+        <input type="text" name="query" id="">
+
+        <input type="submit" name="submit" id=""><br>
+        <div class="radios">
+            <input type="radio" name="prodLine" value=""> none <br>
+            <?php
+            $productLines = Product::getProductLines();
+            for ($i = 0; $i < count($productLines); $i++) {
+            ?>
+                <input type="radio" name="prodLine" value="<?php echo $productLines[$i] ?>"><?php echo $productLines[$i] ?><br>
+            <?php
+            }
+            ?>
+
+        </div>
+    </form>
+    <br>
+
+    <!-- <div class="card">
+        <h3>Name: XXXXXX</h3>
+        <p>Code: XXXXXX</p>
+        <p>Line: XXXXXX</p>
+    </div> -->
+
+    <?php
+    if (isset($_GET["submit"])) {
+
+        $query = null;
+        $prodLine = null;
+
+        if (isset($_GET["query"]) && !empty($_GET["query"])) {
+            $query = $_GET["query"];
+        }
+
+        if (isset($_GET["prodLine"]) && !empty($_GET["prodLine"])) {
+            $prodLine = $_GET["prodLine"];
+        }
+
+        $results = Product::advancedSearch($query, $prodLine);
+
+        if ($results != false) {
+            foreach ($results as $row) {
+    ?>
+                <div class="card">
+                    <h3>Name: <?php echo $row->productName ?></h3>
+                    <p>Code: <?php echo $row->productCode ?></p>
+                    <p>Line: <?php echo $row->productLine ?></p>
+                </div>
+            <?php
+            }
+        }
+    }
+
+    if (!isset($_GET["submit"])) {
+        $results = Product::getAll();
+        if ($results != false) {
+            foreach ($results as $row) {
+            ?>
+                <div class="card">
+                    <h3>Name: <?php echo $row->productName ?></h3>
+                    <p>Code: <?php echo $row->productCode ?></p>
+                    <p>Line: <?php echo $row->productLine ?></p>
+                </div>
+    <?php
+            }
+        }
+    }
+    ?>
+
+    <?php
+    $rowsNum = Product::getSelectCount();
+    $pagesNum = ceil($rowsNum / Product::$rowsPerPage);
+    echo $pagesNum;
+
+    for ($i = 1; $i <= $pagesNum; $i++) {
+    ?>
+        <a href="<?php echo $_SERVER['PHP_SELF'] .  '?' . http_build_query(array_merge($_GET, array("page" => $i))) ?>"><?php echo $i ?></a>
+    <?php
+    }
+    ?>
+
+</body>
+
+</html>
